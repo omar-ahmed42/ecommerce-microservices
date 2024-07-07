7fd8338acca57ec03f28ee99dbb2fff2a1156601
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-07-07 17:12:22 +0300

    Make the generation strategy of category id "auto_increment"
    Temporarily make the generation strategy of category id "auto_increment".

--- catalog/src/main/java/com/omarahmed42/catalog/model/Category.java
+++ catalog/src/main/java/com/omarahmed42/catalog/model/Category.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -18,6 +20,7 @@ import lombok.Setter;
 @Setter
 public class Category implements Serializable {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     @Column(name = "name")
