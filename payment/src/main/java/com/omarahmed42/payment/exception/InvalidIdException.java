e42f742218c5a567cf25ae3661bbf8e44ea689cb
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-07-07 17:34:18 +0300

    Add Snowflake UID generation

---
+++ payment/src/main/java/com/omarahmed42/payment/exception/InvalidIdException.java
@@ -0,0 +1,7 @@
+package com.omarahmed42.payment.exception;
+
+public class InvalidIdException extends RuntimeException {
+    public InvalidIdException(String message) {
+        super(message);
+    }
+}
