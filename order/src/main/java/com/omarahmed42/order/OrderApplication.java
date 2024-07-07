8cdbf32815913e35aee16e6cf4c824491436d2e6
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-07-07 17:28:22 +0300

    Deploy place-order.bpmn

--- order/src/main/java/com/omarahmed42/order/OrderApplication.java
+++ order/src/main/java/com/omarahmed42/order/OrderApplication.java
@@ -3,7 +3,10 @@ package com.omarahmed42.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.camunda.zeebe.spring.client.annotation.Deployment;
+
 @SpringBootApplication
+@Deployment(resources = "classpath:place-order.bpmn")
 public class OrderApplication {
 
 	public static void main(String[] args) {
