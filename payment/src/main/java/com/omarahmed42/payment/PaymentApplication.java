b86d8c5b46452d805ee64ff5165a4b600284865d
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-07-07 17:35:06 +0300

    Deploy BPMN and setup stripe API key

--- payment/src/main/java/com/omarahmed42/payment/PaymentApplication.java
+++ payment/src/main/java/com/omarahmed42/payment/PaymentApplication.java
@@ -1,10 +1,25 @@
 package com.omarahmed42.payment;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.stripe.Stripe;
+
+import io.camunda.zeebe.spring.client.annotation.Deployment;
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
+@Deployment(resources = "classpath:payment-purchase.bpmn")
 public class PaymentApplication {
+
+	@Value("${stripe.endpoint.secret}")
+	private String stripeSecretApiKey;
+
+	@PostConstruct
+	public void setup() {
+		Stripe.apiKey = stripeSecretApiKey;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentApplication.class, args);
