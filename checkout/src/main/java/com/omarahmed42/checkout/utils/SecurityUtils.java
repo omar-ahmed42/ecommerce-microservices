15b6ee68f4e81f381e29585aaf580949cf284f61
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-07-07 17:19:37 +0300

    Add logging to SecurityUtils in checkout service

--- checkout/src/main/java/com/omarahmed42/checkout/utils/SecurityUtils.java
+++ checkout/src/main/java/com/omarahmed42/checkout/utils/SecurityUtils.java
@@ -9,19 +9,24 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
 import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
 
 @UtilityClass
+@Slf4j
 public class SecurityUtils {
 
     public static Authentication getAuthentication() {
+        log.info("Getting authentication");
         return SecurityContextHolder.getContext().getAuthentication();
     }
 
     public static JwtAuthenticationToken getJwtAuthenticationToken() {
+        log.info("Getting JWT");
         return (JwtAuthenticationToken) getAuthentication();
     }
 
     public static String getSubject() {
+        log.info("Getting subject");
         JwtAuthenticationToken token = getJwtAuthenticationToken();
         if (token == null)
             return null;
@@ -29,6 +34,7 @@ public class SecurityUtils {
     }
 
     public static List<GrantedAuthority> getAuthorities() {
+        log.info("Getting authorities");
         JwtAuthenticationToken token = getJwtAuthenticationToken();
         if (token == null)
             return Collections.emptyList();
