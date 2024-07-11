5c93e94adb6c3b007e2f23fdef2757ae6d257c4c
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-07-11 06:30:10 +0300

    Fix a bug that caused exceptions to occur due to duplicate 'id' alias and duplicate results

--- order/src/main/java/com/omarahmed42/order/repository/OrderRepository.java
+++ order/src/main/java/com/omarahmed42/order/repository/OrderRepository.java
@@ -4,12 +4,13 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.omarahmed42.order.model.Order;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    @Query(value = "SELECT * FROM orders o LEFT JOIN order_items oi ON o.id = oi.order_id", nativeQuery = true)
-    Optional<Order> findOrderAndOrderItemsById(Long orderId);
+    @Query(value = "SELECT o.* FROM orders o LEFT JOIN order_items oi ON o.id = oi.order_id WHERE o.id = :order_id", nativeQuery = true)
+    Optional<Order> findOrderAndOrderItemsById(@Param("order_id") Long orderId);
 }
