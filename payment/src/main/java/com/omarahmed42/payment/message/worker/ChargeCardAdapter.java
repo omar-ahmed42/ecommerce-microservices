2c93315b0195a169f90c082f8bc1e6fe7849e8a6
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-07-13 20:01:10 +0300

    Add logging and rename map key to correlationId

--- payment/src/main/java/com/omarahmed42/payment/message/worker/ChargeCardAdapter.java
+++ payment/src/main/java/com/omarahmed42/payment/message/worker/ChargeCardAdapter.java
@@ -5,10 +5,8 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.omarahmed42.payment.dto.request.PaymentRequest;
 import com.omarahmed42.payment.message.payload.RetrievePaymentPayload;
-import com.omarahmed42.payment.message.producer.MessageSender;
 import com.omarahmed42.payment.service.PaymentService;
 
 import io.camunda.zeebe.client.api.response.ActivatedJob;
@@ -22,28 +20,19 @@ import lombok.extern.slf4j.Slf4j;
 public class ChargeCardAdapter {
 
     private final PaymentService paymentService;
-    private final MessageSender messageSender;
-    private final ObjectMapper objectMapper;
 
-    @JobWorker(autoComplete = true, type = "charge-card")
-    public Map<String, String> handle(ActivatedJob job) {
+    @JobWorker(autoComplete = true, type = "charge-card", maxJobsActive = 15)//, timeout = 1000 * 60 * 10)
+    public Map<String, String> handle(final ActivatedJob job) {
+        log.info("Worker Handling charge-card");
         Map<String, Object> variablesAsMap = job.getVariablesAsMap();
         log.info("ChargeCardAdapter variablesAsMap {}", variablesAsMap.toString());
-        // RetrievePaymentPayload payload = objectMapper.convertValue(variablesAsMap, RetrievePaymentPayload.class);
         RetrievePaymentPayload payload = RetrievePaymentPayload.fromMap(variablesAsMap);
 
-        PaymentRequest paymentRequest = PaymentRequest.builder()
-                .orderId(payload.getOrderId())
-                .paymentId(UUID.fromString(payload.getPaymentId()))
-                .totalCost(payload.getTotalCost())
-                .userId(payload.getUserId())
-                .build();
+        PaymentRequest paymentRequest = PaymentRequest.builder().orderId(payload.getOrderId())
+                .paymentId(UUID.fromString(payload.getPaymentId())).totalCost(payload.getTotalCost())
+                .userId(payload.getUserId()).build();
 
-        paymentService.chargeCard(paymentRequest);
-
-        // Message<?> messageToBeSent = new Message("PaymentRetrievedEvent");
-        // messageSender.send(messageToBeSent);
-
-        return Map.of("correlation_id", payload.getCorrelationId());
+        paymentService.chargeCard(paymentRequest, payload.getCorrelationId());
+        return Map.of("correlationId", payload.getCorrelationId());
     }
 }
