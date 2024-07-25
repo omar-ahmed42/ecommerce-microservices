ff7a98d729beea2490f2a2e052db4077b0e594b3
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-07-25 15:14:28 +0300

    Add unit tests for deleting inventory items

--- inventory/src/test/java/com/omarahmed42/inventory/unit/service/impl/InventoryServiceImplTest.java
+++ inventory/src/test/java/com/omarahmed42/inventory/unit/service/impl/InventoryServiceImplTest.java
@@ -1,15 +1,22 @@
 package com.omarahmed42.inventory.unit.service.impl;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.lang.reflect.Method;
 import java.util.Date;
 import java.util.Objects;
+import java.util.Set;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
@@ -25,8 +32,13 @@ import com.omarahmed42.inventory.inventory.model.Inventory;
 import com.omarahmed42.inventory.inventory.repository.InventoryRepository;
 import com.omarahmed42.inventory.inventory.service.impl.InventoryServiceImpl;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+
 @ExtendWith(MockitoExtension.class)
-public class InventoryServiceImplTest {
+class InventoryServiceImplTest {
 
     @Mock
     private InventoryRepository inventoryRepository;
@@ -39,6 +51,14 @@ public class InventoryServiceImplTest {
 
     @InjectMocks
     private InventoryServiceImpl inventoryService;
+
+    private static Validator validator;
+
+    @BeforeAll
+    public static void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
 
     @Tag("addInventoryItem")
     @DisplayName("Adding inventory item (stock should be >= 0 before saving to datastore) -> should succeed")
@@ -72,5 +92,29 @@ public class InventoryServiceImplTest {
         inventory.setCreatedAt(new Date().getTime());
         inventory.setModifiedAt(null);
         return inventory;
+    }
+
+    @Tag("deleteInventory")
+    @DisplayName("Deleting an inventory item")
+    @ParameterizedTest(name = "productId")
+    @ValueSource(longs = { 1L, 2L, 1000L })
+    void deleteInventory_should_delete_successfully(Long productId) {
+        inventoryService.deleteInventory(productId);
+        verify(inventoryRepository).deleteById(anyLong());
+    }
+
+    @Test
+    @Tag("deleteInventory")
+    @DisplayName("Deleting an inventory item with id null should violate constraint")
+    void deleteInventory_null_id_should_violate_constraints() throws NoSuchMethodException, SecurityException {
+        Method deleteInventoryMethod = InventoryServiceImpl.class.getMethod("deleteInventory", Long.class);
+        Object[] parameterValues = { null };
+
+        Set<ConstraintViolation<InventoryServiceImpl>> violations = validator.forExecutables()
+                .validateParameters(inventoryService, deleteInventoryMethod, parameterValues);
+
+        assertTrue(violations.size() > 0);
+        assertEquals("Product ID cannot be empty",
+                ((ConstraintViolation<InventoryServiceImpl>) violations.toArray()[0]).getMessage());
     }
 }
