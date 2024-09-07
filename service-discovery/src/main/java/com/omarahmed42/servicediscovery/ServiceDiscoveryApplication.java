4ba909db4444123294951fdd81968eb4cc7e644b
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-09-07 16:00:42 +0300

    Rename service_discovery package to servicediscovery

--- service-discovery/src/main/java/com/omarahmed42/servicediscovery/ServiceDiscoveryApplication.java
+++ service-discovery/src/main/java/com/omarahmed42/servicediscovery/ServiceDiscoveryApplication.java
@@ -1,4 +1,4 @@
-package com.omarahmed42.service_discovery;
+package com.omarahmed42.servicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
