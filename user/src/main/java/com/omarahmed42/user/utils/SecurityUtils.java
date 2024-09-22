4193ecc984e6cbd6996bf2d83b689327f54ffc72
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-09-23 00:11:47 +0300

    Add authentication checks to SecurityUtils in User Microservice
    - Create isAuthenticated() to check if the user is authenticated.
    - Implement throwIfNotAuthenticated() to throw an exception if the user is not authenticated.

--- user/src/main/java/com/omarahmed42/user/utils/SecurityUtils.java
+++ user/src/main/java/com/omarahmed42/user/utils/SecurityUtils.java
@@ -3,10 +3,13 @@ package com.omarahmed42.user.utils;
 import java.util.Collections;
 import java.util.List;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+
+import com.omarahmed42.user.exception.AuthenticationException;
 
 import lombok.experimental.UtilityClass;
 
@@ -35,4 +38,13 @@ public class SecurityUtils {
         return (List<GrantedAuthority>) getJwtAuthenticationToken().getAuthorities();
     }
 
+    public static boolean isAuthenticated() {
+        return SecurityContextHolder.getContext().getAuthentication() != null
+                && !(SecurityContextHolder.getContext().getAuthentication() instanceof AnonymousAuthenticationToken);
+    }
+
+    public static void throwIfNotAuthenticated() {
+        if (!isAuthenticated())
+            throw new AuthenticationException("Unauthorized: User unauthenticated");
+    }
 }
