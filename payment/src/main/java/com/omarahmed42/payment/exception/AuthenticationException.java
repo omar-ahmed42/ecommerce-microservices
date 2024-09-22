5430b5258d335c4e979666a6357da3a41f1aa910
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-09-23 00:04:28 +0300

    Add authentication checks to SecurityUtils
    - Create isAuthenticated() to check if the user is authenticated.
    - Implement throwIfNotAuthenticated() to throw an exception if the user is not authenticated.

---
+++ payment/src/main/java/com/omarahmed42/payment/exception/AuthenticationException.java
@@ -0,0 +1,8 @@
+package com.omarahmed42.payment.exception;
+
+public class AuthenticationException extends RuntimeException {
+
+    public AuthenticationException(String message) {
+        super(message);
+    }
+}
