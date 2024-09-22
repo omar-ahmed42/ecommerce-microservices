84f290c1819f8bb31b8752dbc9f9e2e15f19e6bb
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-09-22 23:23:19 +0300

    Implement custom Long serialization/deserialization for Jackson
    - Add a JacksonConfiguration class to configure Jackson with custom Long serialization and deserialization.
    - Define LongSerializer to serialize Long values as strings.
    - Define LongDeserializer to deserialize string values back to Longs, handling null values and potential parsing errors.

---
+++ cart/src/main/java/com/omarahmed42/cart/conversion/deserializer/LongDeserializer.java
@@ -0,0 +1,28 @@
+package com.omarahmed42.cart.conversion.deserializer;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+
+public class LongDeserializer extends StdDeserializer<Long> {
+
+    public LongDeserializer() {
+        super(Long.class);
+    }
+
+    @Override
+    public Long deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
+        String value = p.getValueAsString();
+        if (value == null)
+            return null;
+        try {
+            return Long.parseLong(value);
+        } catch (NumberFormatException e) {
+            ctxt.handleUnexpectedToken(_valueType, p);
+            return null;
+        }
+    }
+}
