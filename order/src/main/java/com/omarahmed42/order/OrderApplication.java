5274705725b60a4ad9e5c3d0fd194069ad0262f7
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-09-22 23:51:24 +0300

    Implement entity auditing with Auditable base class
    - Create an Auditable base class with JPA annotations for automatic creation and modification time tracking.
    - Enable JPA auditing in OrderApplication using @EnableJpaAuditing.

--- order/src/main/java/com/omarahmed42/order/OrderApplication.java
+++ order/src/main/java/com/omarahmed42/order/OrderApplication.java
@@ -2,11 +2,13 @@ package com.omarahmed42.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import io.camunda.zeebe.spring.client.annotation.Deployment;
 import net.devh.boot.grpc.client.autoconfigure.GrpcDiscoveryClientAutoConfiguration;
 
 @SpringBootApplication(exclude = { GrpcDiscoveryClientAutoConfiguration.class })
+@EnableJpaAuditing(modifyOnCreate = false)
 @Deployment(resources = "classpath:place-order.bpmn")
 public class OrderApplication {
 
