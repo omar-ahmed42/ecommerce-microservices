c639d3e8e4bd5fa1c470fa6de6e6dcac5ee816f6
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-09-23 00:01:47 +0300

    Implement entity auditing with Auditable base class
    - Create an Auditable base class with JPA annotations for automatic creation and modification time tracking.
    - Enable JPA auditing in PaymentApplication using @EnableJpaAuditing.

--- payment/src/main/java/com/omarahmed42/payment/PaymentApplication.java
+++ payment/src/main/java/com/omarahmed42/payment/PaymentApplication.java
@@ -3,6 +3,7 @@ package com.omarahmed42.payment;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import com.stripe.Stripe;
 
@@ -12,6 +13,7 @@ import net.devh.boot.grpc.client.autoconfigure.GrpcDiscoveryClientAutoConfigurat
 
 @SpringBootApplication(exclude = { GrpcDiscoveryClientAutoConfiguration.class })
 @Deployment(resources = "classpath:payment-purchase.bpmn")
+@EnableJpaAuditing
 public class PaymentApplication {
 
 	@Value("${stripe.api.key}")
