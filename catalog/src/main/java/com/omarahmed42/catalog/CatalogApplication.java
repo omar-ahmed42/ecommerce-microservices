a1bec6d3697833a12861e1b30d1e9c278bf763f9
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-09-22 23:38:14 +0300

    Implement entity auditing with Auditable base class
    - Create an Auditable base class with JPA annotations for automatic creation and modification time tracking.
    - Enable JPA auditing in CatalogApplication using @EnableJpaAuditing.

--- catalog/src/main/java/com/omarahmed42/catalog/CatalogApplication.java
+++ catalog/src/main/java/com/omarahmed42/catalog/CatalogApplication.java
@@ -2,8 +2,10 @@ package com.omarahmed42.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CatalogApplication {
 
 	public static void main(String[] args) {
