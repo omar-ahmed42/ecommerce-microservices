204971e45bbb023516c54358baa2fcd54754b9d7
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2024-09-22 23:19:10 +0300

    Add CORS configuration to API Gateway
    - Adds CORS configuration to allow cross-origin requests.

---
+++ api-gateway/src/main/java/com/omarahmed42/gateway/CorsConfiguration.java
@@ -0,0 +1,25 @@
+package com.omarahmed42.gateway;
+
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfiguration {
+
+    @Bean
+    CorsWebFilter corsFilter() {
+        org.springframework.web.cors.CorsConfiguration corsConfiguration = new org.springframework.web.cors.CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("*"));
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+
+}
